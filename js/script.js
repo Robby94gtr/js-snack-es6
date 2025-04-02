95ae1bce6035c49353d45d7fea227649526b5e49
Robby94gtr <robertogramazio94@gmail.com>
2025-04-02 19:10:51 +0200

    [UPD] Added Array with model of the bike and its weight

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//creare un array di oggetti 
+const bicycles = [
+{
+    //ogni bici deve avere due proprietà : nome e peso
+    name: 'Bianchi Oltre RC',
+    weight: 5,
+} ,
+{
+    name: 'Look 795 Blade RS Proteam',
+    weight: 8,
+} ,
+{
+    name: 'Willier Triestina',
+    weight: 7,
+} ,
+{
+    name: 'Colnago V4Rs',
+    weight: 8,
+} ,
+{
+    name: 'Canyon Ultimate CF SL R101',
+    weight: 6,
+}
+];
+console.log(bicycles);
